10b13f33e0bb313e750e1e0fa77cb2f9cdd2a16e
Michwds <michael.woods@live.ca>
2021-03-15 00:10:37 -0700

    Updated portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, NgModule} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {ContactComponent} from './contact/contact.component';
 import {PortfolioComponent} from './portfolio/portfolio.component';
+import {CoverComponent} from './cover/cover.component';
 import {addImports} from '@angular/compiler-cli/src/ngtsc/transform/src/utils';
 
 @Component({
