ef567b72427c8ed351485d89d3db240cca1f07e6
Michwds <michael.woods@live.ca>
2021-03-15 01:31:59 -0700

    Done for now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {HeaderComponent} from './header/header.component';
 import {ContactComponent} from './contact/contact.component';
 import {PortfolioComponent} from './portfolio/portfolio.component';
 import {CoverComponent} from './cover/cover.component';
+import {Title} from '@angular/platform-browser';
 import {addImports} from '@angular/compiler-cli/src/ngtsc/transform/src/utils';
 
 @Component({
@@ -11,5 +12,13 @@ import {addImports} from '@angular/compiler-cli/src/ngtsc/transform/src/utils';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'portfolio2';
+  title = 'Michael Woods - Portfolio';
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle('Michael Woods - Portfolio');
+  }
+
+  public setTitle(newTitle: string): void {
+    this.titleService.setTitle(newTitle);
+  }
 }
